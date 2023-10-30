5f92da0b53b5a672dedbfe9c6d8bf44ffe043822
seon702 <sean702@naver.com>
2023-10-30 21:15:31 +0900

    전역 예외처리

--- src/main/java/com/itbank/handler/GlobalExceptionHandler.java
+++ src/main/java/com/itbank/handler/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.itbank.handler;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpServerErrorException;
