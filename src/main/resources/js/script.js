d2744b731620378f97d5e36dfc49759a036e5c60
LeeBG <donny1848@gmail.com>
2023-10-31 01:43:04 +0900

    이미지_JS_CSS 파일 추가

---
+++ src/main/resources/js/script.js
@@ -0,0 +1,49 @@
+const info = document.querySelector(".info");
+const title = document.querySelector(".title");
+
+window.addEventListener("DOMContentLoaded", () => {
+	info.style.top = '480px';
+	info.style.left = '1730px';
+}) 
+
+let lastX = 0;
+let lastY = 0; 
+let startX = 0; 
+let startY = 0; 
+
+// 1.
+title.addEventListener('mousedown', function(e){
+  e.preventDefault();  // 기본 이벤트 막기
+  startX = e.clientX;  
+  startY = e.clientY; 
+	
+  // 2.  
+  title.classList.add('active');
+  
+  // 3.
+  document.addEventListener('mouseup', onRemoveEvent); 
+  
+  // 4.
+  document.addEventListener('mousemove', onMove); 
+});
+
+function onRemoveEvent() { 
+  title.classList.remove('active');
+  document.removeEventListener('mouseup', onRemoveEvent); 
+  document.removeEventListener('mousemove', onMove); 
+} 
+
+function onMove(e) { 
+  e.preventDefault(); 
+  lastX = startX - e.clientX; 
+  lastY = startY - e.clientY; 
+
+  startX = e.clientX; 
+  startY = e.clientY; 
+
+  console.log(`${info.offsetTop - lastY}px`);
+  console.log(`${info.offsetLeft - lastX}px`);
+  
+  info.style.top = `${info.offsetTop - lastY}px`;
+  info.style.left = `${info.offsetLeft - lastX}px`;
+}
