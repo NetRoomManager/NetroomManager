087161511efaa41703627ee0755b89f73173dd1c
Soeun21 <131752096+Soeun21@users.noreply.github.com>
2023-11-02 21:35:46 +0900

    11/02-1번 커밋 : 이용권 crud완료

--- src/main/java/com/itbank/controller/AdminController.java
+++ src/main/java/com/itbank/controller/AdminController.java
@@ -1,16 +1,21 @@
 package com.itbank.controller;
 
 import com.itbank.model.Product;
+import com.itbank.model.Ticket;
 import com.itbank.service.ProductService;
+import com.itbank.service.TicketService;
 import com.itbank.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import oracle.jdbc.proxy._Proxy_;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Objects;
 
 @Controller
@@ -23,6 +28,9 @@ public class AdminController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TicketService ticketService;
 
     // 상품관리
     @GetMapping("/product")
@@ -45,9 +53,37 @@ public class AdminController {
 
     // 이용권관리
     @GetMapping("/ticket")
-    public String ticket() {
-        return "/admin/ticket_manage";
+    public ModelAndView ticket() {
+        ModelAndView mav = new ModelAndView("/admin/ticket_manage");
+        List<Ticket> ticketList = ticketService.selectTicketList();
+        mav.addObject("ticketList", ticketList);
+        return mav;
     }
+
+    @PostMapping("/ticketRegister")
+    public String addTicket(Ticket ticket){
+        if(ticket.getId() == null){
+            log.info("이용권 추가 form");
+            int i = ticketService.addTicket(ticket);
+            log.info(i +"개의 이용권이 추가되었습니다");
+        }
+        else {
+            log.info(ticket.getId() + "번 이용권 변경");
+            int i = ticketService.updateTicket(ticket);
+            log.info(i + "개의 이용권이 변경되었습니다 :  이용권 번호 [" + ticket.getId() + "]");
+        }
+        return "redirect:/admin/ticket";
+    }
+
+    @GetMapping("/deleteTicket/{ticketId}")
+    public String deleteTicket(@PathVariable int ticketId){
+        log.info(ticketId + "번 이용권 삭제");
+        int i = ticketService.deleteTicket(ticketId);
+        log.info(i + "개의 이용권을 삭제하였습니다");
+        return "redirect:/admin/ticket";
+    }
+
+
 
     // 상품 매출
     @GetMapping("/productsales")
